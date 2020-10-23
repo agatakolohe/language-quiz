506ea7029337e7b9497f7a141bbc63afc0eea8f7
agatakolohe <agatakolohe@gmail.com>
2020-10-23 08:31:56 -0700

    change all values in inputs to numbers, delete hidden id and span clicks in cards, put click in card title class, edit text for user to click on more fun facts in card, edit radio button layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $("form#quiz").submit(function(event) {
+$(document).ready(function () {
+  $("form#quiz").submit(function (event) {
     event.preventDefault();
 
     const name = $("input#name").val();
@@ -9,35 +9,59 @@ $(document).ready(function() {
     const work = $("input:radio[name=work]:checked").val();
     const helloworld = $("input:radio[name=helloworld]:checked").val();
 
-    if (development === "backend" && color === "red" && build === "web" && work === "github" && helloworld === "rubee") {
+    if (
+      development === "backend" &&
+      color === "red" &&
+      build === "web" &&
+      work === "github" &&
+      helloworld === "rubee"
+    ) {
       $("#csharp, #javascript, #python, #newbie").hide();
       $(".name").text(name);
       $("#ruby").show();
-      $(".clickable").click(function() {
+      $(".clickable").click(function () {
         $("#showing").toggle();
         $("#hidden").toggle();
       });
-    } else if (development === "backend" && color === "purple" && build === "games" && work === "microsoft" && helloworld === "seesharp") {
+    } else if (
+      development === "backend" &&
+      color === "purple" &&
+      build === "games" &&
+      work === "microsoft" &&
+      helloworld === "seesharp"
+    ) {
       $("#ruby, #javascript, #python, #newbie").hide();
       $(".name").text(name);
       $("#csharp").show();
-      $(".c-click").click(function() {
+      $(".c-click").click(function () {
         $("#c-showing").toggle();
         $("#c-hidden").toggle();
       });
-    } else if (development === "frontend" && color === "yellow" && build === "fun" && work === "netflix" && helloworld === "jscript") {
+    } else if (
+      development === "frontend" &&
+      color === "yellow" &&
+      build === "fun" &&
+      work === "netflix" &&
+      helloworld === "jscript"
+    ) {
       $("#ruby, #csharp, #python, #newbie").hide();
       $(".name").text(name);
       $("#javascript").show();
-      $(".j-click").click(function() {
+      $(".j-click").click(function () {
         $("#j-showing").toggle();
         $("#j-hidden").toggle();
       });
-    } else if (development === "backend" && color === "yellowblue" && build === "web" && work === "google" && helloworld === "snake") {
+    } else if (
+      development === "backend" &&
+      color === "yellowblue" &&
+      build === "web" &&
+      work === "google" &&
+      helloworld === "snake"
+    ) {
       $("#ruby, #csharp, #javascript, #newbie").hide();
       $(".name").text(name);
       $("#python").show();
-      $(".p-click").click(function() {
+      $(".p-click").click(function () {
         $("#p-showing").toggle();
         $("#p-hidden").toggle();
       });
@@ -45,7 +69,7 @@ $(document).ready(function() {
       $("#ruby, #csharp, #javascript, #python").hide();
       $(".name").text(name);
       $("#newbie").show();
-      $(".h-click").click(function() {
+      $(".h-click").click(function () {
         $("#h-showing").toggle();
         $("#h-hidden").toggle();
       });
